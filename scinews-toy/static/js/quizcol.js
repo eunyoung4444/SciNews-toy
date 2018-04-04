29e00ff1b268d5b12e191b7ba69cda5a160aff5d
Eunyoung Ko <eunyoungko@kaist.ac.kr>
2018-04-04 22:37:50 +0900

    tutorial script to above'

--- scinews-toy/static/js/quizcol.js
+++ scinews-toy/static/js/quizcol.js
@@ -223,8 +223,7 @@ function reconSidebar(){
         floatingDiv.style.width=parentwidth.toString()+"px";
         }
    });
-} // end of sidebar reconstruction 
-    window.addEventListener('scroll', function(e){})
+} // end of sidebar reconstruction
 
 
 function AfterSelection(){
@@ -380,63 +379,5 @@ function submitQuiz(){
             }else{
                 window.location.replace('../../survey/1');
             }
-}}
-
-/* // 
-function selectToQuestion() {
-        var selectionText = "";
-        if (document.getSelection) {
-            selectionText = document.getSelection();
-            selectionRange= selectionText.getRangeAt(0);
-        } else if (document.selection) {
-            selectionText = document.selection.createRange().text;
-        }
-        if(selectionText){
-            $('#addEbutton')[0].disabled=false;
-        }
+    }
 }
-        
-function snapSelectToQuestion() {
-        var sel;
-        // Check for existence of window.getSelection() and that it has a
-        // modify() method. IE 9 has both selection APIs but no modify() method.
-        if (window.getSelection && (sel = window.getSelection()).modify) {
-            sel = window.getSelection();
-            if (!sel.isCollapsed) {
-    
-                // Detect if selection is backwards
-                var range = document.createRange();
-                range.setStart(sel.anchorNode, sel.anchorOffset);
-                range.setEnd(sel.focusNode, sel.focusOffset);
-                var backwards = range.collapsed;
-                range.detach();
-    
-                // modify() works on the focus of the selection
-                var endNode = sel.focusNode, endOffset = sel.focusOffset;
-                sel.collapse(sel.anchorNode, sel.anchorOffset);
-                
-                var direction = [];
-                if (backwards) {
-                    direction = ['backward', 'forward'];
-                } else {
-                    direction = ['forward', 'backward'];
-                }
-    
-                sel.modify("move", direction[0], "character");
-                sel.modify("move", direction[1], "word");
-                sel.extend(endNode, endOffset);
-                sel.modify("extend", direction[1], "character");
-                sel.modify("extend", direction[0], "word");
-            }
-        } else if ( (sel = document.selection) && sel.type != "Control") {
-            var textRange = sel.createRange();
-            if (textRange.text) {
-                textRange.expand("word");
-                // Move the end back to not include the word's trailing space(s),
-                // if necessary
-                while(/\s$/.text(textRange.text)){
-                    textRange.moveEnd("character",-1);}
-                    textRange.select();
-                }}
-            selectToQuestion();
-} */
